670be35c71801042dbb55c360b293c966358f487
ItsWolfiePlaysMC <martingame665156@gmail.com>
2019-06-27 14:41:46 -0400

    Added the serverinfo command.

--- cogs/general.py
+++ cogs/general.py
@@ -28,5 +28,20 @@ class General(commands.Cog):
         embed.add_field(name = "Links", value = "[Docs](https://dhb-documentation.readthedocs.io/en/latest/index.html) | [Support](https://discord.gg/KEkwrwd) | [Invite](https://discordapp.com/api/oauth2/authorize?client_id=592811241756688405&permissions=2080762998&scope=bot)")
         await ctx.send(embed = embed)
 
+    @commands.command()
+    @commands.guild_only()
+    async def serverinfo(self, ctx):
+        embed = discord.Embed(title = f"Info for {ctx.guild.name}", color = discord.Color.blurple())
+        embed.add_field(name = "Owner", value = ctx.guild.owner)
+        embed.add_field(name = "ID", value = ctx.guild.id)
+        embed.add_field(name = "Region", value = ctx.guild.region)
+        embed.add_field(name = "Categories", value = len(ctx.guild.categories))
+        embed.add_field(name = "Text Channels", value = len(ctx.guild.text_channels))
+        embed.add_field(name = "Voice Channels", value = len(ctx.guild.voice_channels))
+        embed.add_field(name = "Members", value = len(ctx.guild.member_count))
+        embed.add_field(name = "Roles", value = len(ctx.guild.roles))
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        await ctx.send(embed = embed)
+
 def setup(bot):
     bot.add_cog(General(bot))
