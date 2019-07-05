687e89a0a5aa16a34acc4a4d3169689a814f3be7
A Trash Coder <38404553+A-Trash-Coder@users.noreply.github.com>
2019-07-05 13:48:52 -0400

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-token = "tokenhere"
-dbhost = "dbhostiphere" # or localhost, I put it there just incase.
-dbuser = "dbuserhere"
-dbuserpasswd = "dbuserpasswordhere"
-dboatstoken = "discordboatstokenhere"
-bfdtoken = "botsfordiscordtokenhere"
